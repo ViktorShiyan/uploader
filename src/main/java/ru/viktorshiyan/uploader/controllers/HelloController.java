683690d4c63a0ad1a577ee2220bd61f880af020f
Victor Shiyan <VictorShyan@yandex.ru>
2022-10-12 18:32:01 +0300

    Add swagger

---
+++ src/main/java/ru/viktorshiyan/uploader/controllers/HelloController.java
@@ -0,0 +1,12 @@
+package ru.viktorshiyan.uploader.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello. This is Uploader";
+    }
+}
